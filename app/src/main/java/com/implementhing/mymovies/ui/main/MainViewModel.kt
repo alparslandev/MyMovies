46fbc2adc14d8917bc7087e40ab5ef94ec859f05
Alparslan Develioğlu <alparslanselcuk.develioglu@sekerbank.com.tr>
2020-12-28 06:55:04 +0300

    SearchMovie service connection is added to MainViewModel

--- app/src/main/java/com/implementhing/mymovies/ui/main/MainViewModel.kt
+++ app/src/main/java/com/implementhing/mymovies/ui/main/MainViewModel.kt
@@ -1,8 +1,8 @@
 package com.implementhing.mymovies.ui.main
 
+import android.text.Editable
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.implementhing.data.models.Movie
-import com.implementhing.data.models.base.MovieBaseResponse
 import com.implementhing.data.models.request.SearchMovieRequestModel
 import com.implementhing.domain.SearchUseCase
 import com.implementhing.presentation.BaseViewModel
@@ -16,15 +16,30 @@ class MainViewModel @Inject constructor(
     private val searchUseCase: SearchUseCase,
 ) : BaseViewModel() {
 
+    var query = MutableLiveData<String>()
     var presenter: Presenter? = null
+
+    fun searchMovies(editable: Editable) {
+        if (editable.length > 2)
+            searchMovies(editable.toString())
+    }
 
     fun searchMovies(query: String) {
         viewModelScope.launch {
             searchUseCase
-                .invoke(SearchMovieRequestModel(query))
-                .collect {
+                .invoke(SearchMovieRequestModel(query)).collect {
+                    val movieUIModels = mutableListOf<MovieUIModel>()
                     it.results?.let { movies ->
-                        presenter?.presentList(movies)
+                        movies.forEach { movie ->
+                            movieUIModels.add(
+                                MovieUIModel(
+                                    vote = "${movie.voteAverage}",
+                                    title = movie.title,
+                                    imagePath = "https://image.tmdb.org/t/p/w500" + movie.posterPath
+                                )
+                            )
+                        }
+                        presenter?.presentList(movieUIModels)
                     }
                 }
         }
@@ -32,6 +47,6 @@ class MainViewModel @Inject constructor(
 
     interface Presenter {
         fun navigateToDetailsPage()
-        fun presentList(results: List<MovieBaseResponse<Movie>>)
+        fun presentList(movies: MutableList<MovieUIModel>)
     }
 }
