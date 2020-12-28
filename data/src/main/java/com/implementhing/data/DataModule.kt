cfa0c625920500c7494055e4326e615f44c7f053
Alparslan Develioğlu <alparslanselcuk.develioglu@sekerbank.com.tr>
2020-12-28 03:30:43 +0300

    Improvements on DataModule

--- data/src/main/java/com/implementhing/data/DataModule.kt
+++ data/src/main/java/com/implementhing/data/DataModule.kt
@@ -6,9 +6,6 @@ import com.implementhing.common.infrastructure.log.LogBucket
 import com.implementhing.data.core.FlowCallAdapterFactory
 import com.implementhing.data.core.interceptors.CommonQueryParamsInterceptor
 import com.implementhing.data.core.interceptors.HeadersInterceptor
-import com.implementhing.data.services.search.SearchRepository
-import com.implementhing.data.services.search.SearchRepositoryImpl
-import com.implementhing.data.services.search.SearchService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,79 +14,40 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Qualifier
 import javax.inject.Singleton
-
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object DataModule {
 
-    @MustBeDocumented
-    @Qualifier
-    annotation class Movies
-
-    @MustBeDocumented
-    @Qualifier
-    annotation class Search
-
     @Provides
-    fun provideRetrofitBuilder(gson: Gson, http: OkHttpClient): Retrofit.Builder {
-        return Retrofit
-                .Builder()
-                .client(http)
-                .addCallAdapterFactory(FlowCallAdapterFactory())
-                .addConverterFactory(GsonConverterFactory.create(gson))
-    }
-
-    @Provides
-    @Singleton
-    @Movies
-    fun provideMoviesService(retrofitBuilder: Retrofit.Builder, configuration: Configuration): MoviesService {
-        return retrofitBuilder
-                .baseUrl(configuration.apiUrl() + configuration.apiVersion() + "movie/")
-                .build()
-                .create(MoviesService::class.java)
-    }
-
-    @Provides
-    @Singleton
-    @Movies
-    fun provideMoviesRepository(moviesService: MoviesService): MoviesRepository {
-        return MoviesRepositoryImpl(moviesService)
-    }
-
-    @Provides
-    @Singleton
-    @Search
-    fun provideSearchService(retrofitBuilder: Retrofit.Builder, configuration: Configuration): SearchService {
-        return retrofitBuilder
-                .baseUrl(configuration.apiUrl() + configuration.apiVersion() + "search/")
-                .build()
-                .create(SearchService::class.java)
-    }
-
-    @Provides
-    @Singleton
-    @Search
-    fun provideSearchRepository(searchService: SearchService): SearchRepository {
-        return SearchRepositoryImpl(searchService)
-    }
-
-    @Provides
-    @Singleton
-    fun provideHttp(logBucket: LogBucket, configuration: Configuration): OkHttpClient {
+    fun provideHttp(logBucket: LogBucket, configuration: Configuration, gson: Gson): ApiService {
         val log = HttpLoggingInterceptor {
             logBucket.debug(it)
         }
 
         log.level = HttpLoggingInterceptor.Level.BODY
 
-        return OkHttpClient
+        val http = OkHttpClient
                 .Builder()
                 .addInterceptor(log)
                 .addInterceptor(HeadersInterceptor())
                 .addInterceptor(CommonQueryParamsInterceptor(configuration))
                 .build()
+
+        return Retrofit
+            .Builder()
+            .client(http)
+            .addCallAdapterFactory(FlowCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(configuration.apiUrl() + configuration.apiVersion())
+            .build()
+            .create(ApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSearchRepository(apiService: ApiService): ApiRepository {
+        return ApiRepositoryImpl(apiService)
     }
 }
